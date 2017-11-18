648a0fe9a1f42d4b3df8c87a71f4f69754ce9cfc
Baris OZER <barisozer@hurriyet.com.tr>
2017-11-18 11:39:41 +0300

    Project created

---
+++ FlickrImageGallery/FlickrImageGallery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickrImageGallery
+//
+//  Created by Barış Özer on 11/18/17.
+//  Copyright © 2017 Tigerspike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
